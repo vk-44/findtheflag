1b825c0c59f7969671f2e93b0e647fb88fde867f
Vikram <vikramgreenliker44@gmail.com>
2024-06-01 11:39:28 +0530

    Added new icons

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,8 +1,10 @@
-import { Flag, Star, Trophy,RotateCw } from "lucide-react";
+import { Flag, Star, Trophy, RotateCw, Award, Palette } from "lucide-react";
 
 const iconStar = <Star className="iconStar" />;
 const iconFlag = <Flag className="iconFlag" />;
 const iconTrophy = <Trophy className="iconTrophy" />;
 const iconRotate = <RotateCw className="iconRotate" />;
+const iconAward = <Award className="iconAward" />;
+const iconPalette = <Palette className="iconPalette" />;
 
-export { iconStar, iconFlag, iconTrophy, iconRotate };
+export { iconStar, iconFlag, iconTrophy, iconRotate, iconAward, iconPalette };
