c04fd8abdabfeda80a27b9c211bdd465edfa4880
Vikram <vikramgreenliker44@gmail.com>
2024-06-01 11:46:05 +0530

    Changed game mode functionality, added theme button

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -2,7 +2,7 @@ import { FlagTriangleRight } from "lucide-react";
 import { useGlobal } from "../Context/GlobalContext";
 import { useMemo } from "react";
 import SettingsItem from "./SettingsItem";
-import { iconRotate } from "./Icons";
+import { iconRotate, iconPalette } from "./Icons";
 
 function Settings() {
   const { dispatch, isEasyMode } = useGlobal();
@@ -20,7 +20,15 @@ function Settings() {
       <SettingsItem
         icon={iconFlagTriangle}
         description={isEasyMode ? "Easy" : "Hard"}
-        onClick={() => dispatch({ type: "gameModeChanged" })}
+        onClick={() => {
+          dispatch({ type: "gameModeChanged" });
+          dispatch({ type: "restartClicked" });
+        }}
+      />
+      <SettingsItem
+        icon={iconPalette}
+        description={"Theme"}
+        onClick={() => console.log("theme changed")}
       />
       <SettingsItem
         icon={iconRotate}
