afbe4ec4fbd5c1e3e883a6a88e82b0963b1cacf8
Vikram <vikramgreenliker44@gmail.com>
2024-06-01 11:39:12 +0530

    Added title and new highscore

--- src/components/End.jsx
+++ src/components/End.jsx
@@ -1,19 +1,43 @@
 import { useGlobal } from "../Context/GlobalContext";
-import { iconStar, iconTrophy, iconRotate } from "./Icons";
+import { iconStar, iconTrophy, iconRotate, iconAward } from "./Icons";
 import StatusItem from "./StatusItem";
 import SettingsItem from "./SettingsItem";
 
 function End() {
-  const { dispatch, points, highscore } = useGlobal();
+  const { dispatch, points, highscore, isNewHighscore } = useGlobal();
+  let title;
+  if (points <= 40) {
+    title = "Novice Navigator";
+  } else if (points <= 80) {
+    title = "Competent Cartographer";
+  } else if (points <= 120) {
+    title = "Proficient Pathfinder";
+  } else if (points <= 160) {
+    title = "Legendary Locator";
+  } else if (points < 194) {
+    title = "Master Mapmaker";
+  } else if (points === 194) {
+    title = "Ultimate Geographer";
+  }
+
   return (
     <div className="end">
       <div className="endStatusContainer">
+        <p className="title">
+          <span>You earned the title</span> <b>{title}</b>
+        </p>
         <StatusItem icon={iconStar} description={"Score"} data={points} />
         <StatusItem
           icon={iconTrophy}
           description={"Highscore"}
           data={highscore}
         />
+        {isNewHighscore && (
+          <p className="descriptionContainer newHighScore">
+            {iconAward}
+            <span>New Highscore!</span>
+          </p>
+        )}
         <SettingsItem
           icon={iconRotate}
           description={"Restart"}
