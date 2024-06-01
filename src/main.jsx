598a605805865c0d9f7698e5ccddc9f936932410
Vikram <vikramgreenliker44@gmail.com>
2024-06-01 11:41:13 +0530

    Style file for icons

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./Styles/index.css";
 import "./Styles/media.css";
+import "./Styles/icons.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
